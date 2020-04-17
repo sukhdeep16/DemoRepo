1f34ab0b0a6f83d042849f566b2504a9e12e5f97
sukhi16 <sukhdeep.10220@gmail.com>
2020-04-17 16:54:53 +0530

    Mine First GIT commit for Selenium Code

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,9 @@
+package com.qa.pages;
+
+public class Homepage {
+	
+	public void homePage() {
+		System.out.println("Home Page");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/LoginPageTest.java
@@ -0,0 +1,12 @@
+package com.qa.tests;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	public void test1() {
+		System.out.println("Test 1");
+	}
+ 
+}
